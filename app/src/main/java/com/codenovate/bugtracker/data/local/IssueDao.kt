7e233c4594fc3783be5e9fc8abdff5bc250a25bc
Thankgod Ikari <thankgodikari@my.uopeople.edu>
2026-07-08 23:53:42 +0800

    Implement Room DAO CRUD operations

--- app/src/main/java/com/codenovate/bugtracker/data/local/IssueDao.kt
+++ app/src/main/java/com/codenovate/bugtracker/data/local/IssueDao.kt
@@ -1,15 +1,33 @@
 package com.codenovate.bugtracker.data.local
 
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+
+@Dao
 interface IssueDao {
 
+
+    @Insert
     fun insert(issue: IssueEntity)
 
+
+    @Update
     fun update(issue: IssueEntity)
 
+
+    @Delete
     fun delete(issue: IssueEntity)
 
+
+    @Query("SELECT * FROM issues")
     fun getAll(): List<IssueEntity>
 
+
+    @Query("SELECT * FROM issues WHERE id = :id")
     fun getById(id: Int): IssueEntity?
 
 }
