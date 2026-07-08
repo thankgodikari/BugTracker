201366fb1b1f60533de0f2c08a24e3f4274e73b5
Thankgod Ikari <thankgodikari@my.uopeople.edu>
2026-07-09 00:31:27 +0800

    Add WorkManager dependency for background synchronization

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,4 +50,5 @@ dependencies {
 
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.work.runtime.ktx)
 }
